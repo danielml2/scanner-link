af5f406181ad9ac965650ce70420463a0b5d086d
Daniel Hackmon <92460461+DanielChakmon@users.noreply.github.com>
2024-05-28 12:40:43 +0300

    Update Polkadot.ts

--- coins/Polkadot.ts
+++ coins/Polkadot.ts
@@ -3,11 +3,24 @@ import { Coin } from "./Coin";
 export class Polkadot implements Coin{
     name='Polkadot';
 
-
     isValid(address: string){
-        return (address.length==48);
+        const apiUrl = `https://polkadot-public-sidecar.parity-chains.parity.io/accounts/${this.address}/validate`;
+        fetch(apiUrl)
+    
+            .then(response => {
+                  if (!response.ok) {
+                       throw new Error('Network response was not ok');
+                     }
+               return response.json();
+             })
+            .then(data => {
+                  this.isValid=data.isValid;
+                 })
+            .catch(error => {
+                  console.error('Error:', error);
+            });
+        return this.isValid;
     }
-
 
     getURL(address: string){
         return `https://polkadot.subscan.io/account/${address}`;
